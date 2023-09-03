79039d77109f2874022812a4845956f0bf5867ce
BackstagecrewIS <locksmith@backstagecrew.com>
2023-09-03 19:39:59 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_YoqlQMltjGOQF7CB7T6HfFIJ00fJ4QP2x9',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
