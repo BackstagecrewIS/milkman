31278911b4fa14026614583bf6575ba237ef5340
BackstagecrewIS <locksmith@backstagecrew.com>
2023-10-07 00:36:29 +0000

    Debugging Settings for Heroku

--- milkman/settings.py
+++ milkman/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['app-milkman-1c8cec7719a8.herokuapp.com/','localhost']
+ALLOWED_HOSTS = ['https://app-milkman-1c8cec7719a8.herokuapp.com/','localhost']
 
 
 # Application definition
