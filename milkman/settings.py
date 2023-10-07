08f3aa2a5157df4d136dcae3d1c38cdf2e94c11f
BackstagecrewIS <locksmith@backstagecrew.com>
2023-10-07 16:35:21 +0000

    Debugging AWS Settings

--- milkman/settings.py
+++ milkman/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['app-milkman-1c8cec7719a8.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['app-milkman-1c8cec7719a8.herokuapp.com','8000-backstagecrewis-milkman-gv8e3vmyqxk.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -182,7 +182,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.eu-west-2.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
